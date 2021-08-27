44e515af731c7ec3811770d75a074869680ce315
Joseph Canlas <49482845+iudexraphael@users.noreply.github.com>
2021-08-27 10:41:52 +0800

    Activity 1 Files

---
+++ XamarinActivity1/XamarinActivity1/XamarinActivity1/AppConstants.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinActivity1
+{
+   static class AppConstants
+    {
+        public static double NormalFontSize = 14;
+
+        public static double LargeFontSize = 25;
+
+        public static double SmallFontSize = 12;
+
+        public static double TinyFontSize = 6;
+    }
+}
